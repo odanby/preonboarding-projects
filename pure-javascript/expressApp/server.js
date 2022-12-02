96866cafaa5e56a1ce29bcd3631bfee011a940a5
odanby <oriana.danby@gmail.com>
2022-12-02 13:26:25 -0800

    brain is a little fried today, will try again l8r

--- pure-javascript/expressApp/server.js
+++ pure-javascript/expressApp/server.js
@@ -6,6 +6,38 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
+// let's see if i can move database here
+const {Client} = require("pg");
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "Hoku501046!",
+    database: "cow_escape"
+});
+
+client.connect();
+
+client.query(`Select * from highscores`, (err, res) => {
+    if(!err){
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end;
+});
+
+client.query(`Select * from gamer_comments`, (err, res) => {
+    if(!err){
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end;
+});
+// end of new database
+
 app.get("/", (req, res) => {
     res.render("index")
 })
@@ -14,12 +46,21 @@ app.get("/play", (req, res) => {
     res.render("game")
 });
 
-const highscoreRouter = require("./routes/highscores");
-const commentRouter = require("./routes/comments");
+app.get("/highscore", (req, res) => {
+    res.send("All high scores")
+});
 
-app.use("/highscore", highscoreRouter);
-app.use("/comments", commentRouter);
+app.post("/highscore/new", (req, res) => {
+    res.send("Post a new high score")
+})
 
+app.get("/comment", (req, res) => {
+    res.send("All comments")
+});
+
+app.post("/comment/new", (req,res) => {
+    res.send("Post a new comment")
+});
 
 
 app.listen(3000);
